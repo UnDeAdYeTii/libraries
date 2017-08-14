7b5e9a9c2fc5d919177195bb235bd577720b6f72
UnDeAd-YeTii <bradietilley97@gmail.com>
2017-08-14 19:25:45 +0800

    fixes

--- src/YeTii/Applications/Curl.php
+++ src/YeTii/Applications/Curl.php
@@ -10,16 +10,16 @@ class Curl {
 	private $methods_available = array('POST','GET','PUT','DELETE','PATCH','OPTIONS');
 	private $data = [];
 	private $timeout = 30;
-	private $returntransfer = TRUE;
+	private $returntransfer = true;
 	private $encoding = '';
 	private $maxredirs = 10;
-	private $ssl_verifyhost = FALSE;
-	private $ssl_verifypeer = FALSE;
-	private $jsondecode_result = TRUE;
+	private $ssl_verifyhost = false;
+	private $ssl_verifypeer = false;
+	private $jsondecode_result = true;
 
-	protected $error;
-	protected $success;
-	protected $response;
+	protected $error = null;
+	protected $success = null;
+	protected $response = null;
 
 	function __construct(array $arr = []) {
 		foreach ($arr as $key => $value) {
@@ -76,7 +76,7 @@ class Curl {
 		return $this;
 	}
 	public function returntransfer($value) {
-		$this->returntransfer = $value ? TRUE : FALSE;
+		$this->returntransfer = $value ? true : false;
 		return $this;
 	}
 	public function encoding($value) {
@@ -88,11 +88,11 @@ class Curl {
 		return $this;
 	}
 	public function ssl_verifyhost($value) {
-		$this->ssl_verifyhost = $value ? TRUE : FALSE;
+		$this->ssl_verifyhost = $value ? true : false;
 		return $this;
 	}
 	public function ssl_verifypeer($value) {
-		$this->ssl_verifypeer = $value ? TRUE : FALSE;
+		$this->ssl_verifypeer = $value ? true : false;
 		return $this;
 	}
 	public function fetch($url = null) {
@@ -111,26 +111,32 @@ class Curl {
 		return $this->success;
 	}
 
+	private function compile_url() {
+		if (!$this->data || in_array($this->method, array('POST','PUT','PATCH'))) return $this->url;
+		return $this->url.'?'.http_build_query($this->data);
+	}
+
 	private function do_curl() {
+		if (!$this->url) return false;
 		$this->success = false;
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
-			CURLOPT_URL => $this->url,
+			CURLOPT_URL => $this->compile_url(),
 			CURLOPT_RETURNTRANSFER => $this->returntransfer,
 			CURLOPT_ENCODING => $this->encoding,
 			CURLOPT_MAXREDIRS => $this->maxredirs,
 			CURLOPT_TIMEOUT => $this->timeout ? $this->timeout : 30,
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 			CURLOPT_CUSTOMREQUEST => $this->method,
-			CURLOPT_POSTFIELDS => $this->data ? $this->data : "{}",
 			CURLOPT_SSL_VERIFYHOST => $this->ssl_verifyhost,
 			CURLOPT_SSL_VERIFYPEER => $this->ssl_verifypeer
 		));
-		// $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		// $errorno = curl_errno($curl);
+		if (in_array($this->method, array('POST','PUT','PATCH')))
+			curl_setopt($curl, CURLOPT_POSTFIELDS, ($this->data ? http_build_query($this->data) : "{}"));
 		$this->response = curl_exec($curl);
 		$this->error = curl_error($curl);
 		$this->success = $this->response && !$this->error ? true : false;
+		if (!$this->success) $this->response = null;
 		if ($this->jsondecode_result)
 			$this->jsonify();
 	}
@@ -140,10 +146,6 @@ class Curl {
 		if (preg_match('/^\{.+\}$/', $this->response)) {
 			$this->response = json_decode($this->response);
 			$this->success = true;
-		}else{
-			$this->error .= "\nInvalid JSON. Failed to jsonify";
-			$this->success = false;
-			$this->response = FALSE;
 		}
 		return $this;
 	}
