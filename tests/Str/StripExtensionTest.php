0cadacedcff5b4b381f8fc61e2944c2563ae6d36
pxgamer <owzie123@gmail.com>
2017-10-15 23:33:14 +0100

    Added tests for the Str::stripExtension() method

---
+++ tests/Str/StripExtensionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class StripExtensionTest
+ */
+class StripExtensionTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball.txt';
+
+    /**
+     * Test whether it returns a string with the extension stripped
+     */
+    public function testHtml()
+    {
+        $str = new Str($this->testString);
+        $result = $str->stripExtension();
+        $this->assertTrue($result->value === 'The quick brown fox jumps over the lazy dog, Snowball');
+    }
+}
