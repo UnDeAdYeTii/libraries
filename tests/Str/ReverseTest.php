6803ef4cd64d26cc8a4a971060eaa5ca1388cdd1
pxgamer <owzie123@gmail.com>
2017-10-15 23:31:15 +0100

    Added tests for the Str::reverse() method

---
+++ tests/Str/ReverseTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class ReverseTest
+ */
+class ReverseTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it returns a reversed string
+     */
+    public function testStringReverse()
+    {
+        $str = new Str($this->testString);
+        $result = $str->reverse();
+        $this->assertTrue($result->value === 'llabwonS ,god yzal eht revo spmuj xof nworb kciuq ehT');
+    }
+}
