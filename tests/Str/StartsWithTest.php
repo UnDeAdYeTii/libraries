026ae6743fb22846b3c9c202ad3a4dc1bc515c90
pxgamer <owzie123@gmail.com>
2017-10-15 20:11:28 +0100

    Added tests for the Str::startsWith() method

---
+++ tests/Str/StartsWithTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class StartsWithTest
+ */
+class StartsWithTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it starts with a string using the correct case
+     */
+    public function testStartsWithCorrectCase()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->startsWith('The') === true);
+    }
+
+    /**
+     * Test whether it starts with a string using the incorrect case
+     */
+    public function testStartsWithIncorrectCaseWithoutIgnore()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->startsWith('the') === false);
+    }
+
+    /**
+     * Test whether it starts with a string using the incorrect case with case-insensitive
+     */
+    public function testStartsWithIncorrectCaseWithIgnore()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->startsWith('the', true) === true);
+    }
+}
