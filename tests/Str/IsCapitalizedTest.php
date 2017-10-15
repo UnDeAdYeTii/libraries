a7a10e596dd6f4e414b83ba5de9ace9db18375b7
pxgamer <owzie123@gmail.com>
2017-10-15 21:21:49 +0100

    Added tests for the Str::isCapitalized() method

---
+++ tests/Str/IsCapitalizedTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class IsCapitalizedTest
+ */
+class IsCapitalizedTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it starts with a capital letter
+     */
+    public function testIsCapitalized()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->isCapitalized());
+    }
+}
