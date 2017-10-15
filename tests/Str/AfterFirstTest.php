fce60f464a0ed3577ae149970bd629a4036a11b9
pxgamer <owzie123@gmail.com>
2017-10-15 21:02:35 +0100

    Added tests for the Str::afterFirst() method

---
+++ tests/Str/AfterFirstTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class AfterFirstTest
+ */
+class AfterFirstTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it has the correct result after a string using the correct case
+     */
+    public function testAfterFirstCorrectCase()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->afterFirst('s');
+        $this->assertInstanceOf(Str::class, $str2);
+        $this->assertTrue($str2->value === ' over the lazy dog, Snowball');
+    }
+
+    /**
+     * Test whether it has the correct result after a string string using the incorrect case
+     */
+    public function testAfterFirstIncorrectCaseWithoutIgnore()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->afterFirst('S');
+        $this->assertInstanceOf(Str::class, $str2);
+        $this->assertTrue($str2->value !== ' over the lazy dog, Snowball');
+    }
+
+    /**
+     * Test whether it has the correct result after a string using the incorrect case with case-insensitive
+     */
+    public function testAfterFirstIncorrectCaseWithIgnore()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->afterFirst('S', true);
+        $this->assertInstanceOf(Str::class, $str2);
+        $this->assertTrue($str2->value === ' over the lazy dog, Snowball');
+    }
+}
