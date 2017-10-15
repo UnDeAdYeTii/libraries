82ad404a426e2b0da5c6b1445970987b9ab54f15
pxgamer <owzie123@gmail.com>
2017-10-15 22:52:24 +0100

    Added tests for the Str::equals() method

---
+++ tests/Str/EqualsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class EqualsTest
+ */
+class EqualsTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it is equal
+     */
+    public function testEquals()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->equals($this->testString, true));
+    }
+
+    /**
+     * Test whether it is equal with an invalid string
+     */
+    public function testEqualsInvalidString()
+    {
+        $str = new Str($this->testString);
+        $this->assertFalse($str->equals('THE quick brown fox jumps over the lazy dog, SNOWBALL'));
+    }
+
+    /**
+     * Test whether it is equal with an invalid string and ignore
+     */
+    public function testEqualsInvalidStringWithIgnore()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->equals('THE quick brown fox jumps over the lazy dog, SNOWBALL', true));
+    }
+}
