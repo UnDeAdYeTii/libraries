166481882828d843b34fdc1b605bd40c7fc4b0bc
pxgamer <owzie123@gmail.com>
2017-10-15 21:18:07 +0100

    Added tests for the Str::toLowerCase() method

---
+++ tests/Str/ToLowerCaseTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class ToLowerCaseTest
+ */
+class ToLowerCaseTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it returns a lower case string
+     */
+    public function testToLowerCase()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->toLowerCase();
+        $this->assertTrue($str2->value === strtolower($this->testString));
+    }
+}
