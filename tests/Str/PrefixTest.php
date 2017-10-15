12cbc6c5b0b89e5b54f24fd77f5fa7f4c54ef7b3
pxgamer <owzie123@gmail.com>
2017-10-15 22:24:23 +0100

    Added tests for the Str::prefix() method

---
+++ tests/Str/PrefixTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class PrefixTest
+ */
+class PrefixTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it prefixes correctly
+     */
+    public function testPrefix()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->prefix('the ');
+        $this->assertTrue($str2->value === 'the ' . $this->testString);
+    }
+
+    /**
+     * Test whether it prefixes with existing
+     */
+    public function testPrefixIfNotExists()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->prefix('the ', true);
+        $this->assertTrue($str2->value === 'the ' . $this->testString);
+    }
+
+    /**
+     * Test whether it prefixes while not ignoring existing
+     */
+    public function testPrefixIfNotExistsWithoutIgnore()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->prefix('the ', false, true);
+        $this->assertTrue($str2->value === 'the ' . $this->testString);
+    }
+
+    /**
+     * Test whether it prefixes while ignoring existing
+     */
+    public function testPrefixIfNotExistsWithIgnore()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->prefix('the ', true, true);
+        $this->assertFalse($str2->value === 'the ' . $this->testString);
+    }
+}
