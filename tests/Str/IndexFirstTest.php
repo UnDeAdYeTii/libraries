2d975f81686fae7c255fb370ec4c7b262a877ab5
pxgamer <owzie123@gmail.com>
2017-10-15 20:05:49 +0100

    Added tests for the Str::indexFirst() method

---
+++ tests/Str/IndexFirstTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class IndexFirstTest
+ */
+class IndexFirstTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it has a first index using the correct case
+     */
+    public function testIndexFirstCorrectCase()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->indexFirst('T') === 0);
+    }
+
+    /**
+     * Test whether it doesn't have a first index using the incorrect case
+     */
+    public function testIndexFirstIncorrectCaseWithoutIgnore()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->indexFirst('t') !== 0);
+    }
+
+    /**
+     * Test whether it has a first index using the incorrect case with case-insensitive
+     */
+    public function testIndexFirstIncorrectCaseWithIgnore()
+    {
+        $str = new Str($this->testString);
+        $this->assertTrue($str->indexFirst('t', true) === 0);
+    }
+}
