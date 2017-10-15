9dc80f105037805de24fa3e79f3e1849aca62d22
pxgamer <owzie123@gmail.com>
2017-10-15 22:44:32 +0100

    Added tests for the Str::betweenLazy() method

---
+++ tests/Str/BetweenLazyTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class BetweenLazyTest
+ */
+class BetweenLazyTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it returns lazily between two strings
+     */
+    public function testBetweenLazy()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->betweenLazy('e', 'S');
+        $this->assertTrue($str2->value === ' lazy dog, ');
+    }
+
+    /**
+     * Test whether it returns lazily between two incorrect strings
+     */
+    public function testBetweenLazyInvalidStrings()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->betweenLazy('e', 's');
+        $this->assertFalse($str2->value === ' lazy dog, ');
+    }
+
+    /**
+     * Test whether it returns lazily between two strings with ignore
+     */
+    public function testBetweenLazyInvalidStringsWithIgnore()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->betweenLazy('e', 's', true);
+        $this->assertTrue($str2->value === ' lazy dog, ');
+    }
+}
