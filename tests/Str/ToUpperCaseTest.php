e6bc82037020dc6da6ebb7e269394031caa1f51a
pxgamer <owzie123@gmail.com>
2017-10-15 21:18:57 +0100

    Added tests for the Str::toUpperCase() method

---
+++ tests/Str/ToUpperCaseTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class ToUpperCaseTest
+ */
+class ToUpperCaseTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it returns an upper case string
+     */
+    public function testToUpperCase()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->toUpperCase();
+        $this->assertTrue($str2->value === strtoupper($this->testString));
+    }
+}
