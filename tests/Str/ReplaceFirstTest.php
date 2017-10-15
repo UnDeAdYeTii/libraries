44d8adb7d4fb313b4cc572889bfad653096fa684
pxgamer <owzie123@gmail.com>
2017-10-15 22:06:22 +0100

    Added tests for the Str::replaceFirst() method

---
+++ tests/Str/ReplaceFirstTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class ReplaceFirstTest
+ */
+class ReplaceFirstTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it replaces the first instance correctly using a correct case
+     */
+    public function testReplaceFirstCorrectCase()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->replaceFirst('The', 'A');
+        $this->assertTrue($str2->value === 'A quick brown fox jumps over the lazy dog, Snowball');
+    }
+
+    /**
+     * Test whether it replaces the first instance correctly using an incorrect case
+     */
+    public function testReplaceFirstIncorrectCaseWithoutIgnore()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->replaceFirst('the', 'A');
+        $this->assertFalse($str2->value === 'A quick brown fox jumps over the lazy dog, Snowball');
+    }
+
+    /**
+     * Test whether it replaces the first instance correctly using an incorrect case with ignore
+     */
+    public function testReplaceFirstIncorrectCaseWithIgnore()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->replaceFirst('the', 'A', true);
+        $this->assertTrue($str2->value === 'A quick brown fox jumps over the lazy dog, Snowball');
+    }
+}
