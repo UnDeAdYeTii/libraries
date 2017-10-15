377d76220a289b4d624941724308d5fde6c1f3dd
pxgamer <owzie123@gmail.com>
2017-10-15 21:26:43 +0100

    Added tests for the Str::capitalizeWords() method

---
+++ tests/Str/CapitalizeWordsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use YeTii\General\Str;
+
+/**
+ * Class CapitalizeWordsTest
+ */
+class CapitalizeWordsTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    public $testString = 'The quick brown fox jumps over the lazy dog, Snowball';
+
+    /**
+     * Test whether it will capitalize the words
+     */
+    public function testCapitalizeWords()
+    {
+        $str = new Str($this->testString);
+        $str2 = $str->capitalizeWords();
+        $this->assertTrue($str2->value === ucwords($this->testString));
+    }
+}
