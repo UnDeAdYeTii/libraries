32e682fa19175feb087b0f736d627be4c73fcbf2
UnDeAd-YeTii <bradietilley97@gmail.com>
2017-10-16 18:45:10 +0800

    AfterFirstTest fix

--- tests/Str/AfterFirstTest.php
+++ tests/Str/AfterFirstTest.php
@@ -27,18 +27,18 @@ class AfterFirstTest extends TestCase
     /**
      * Test whether it has the correct result after a string string using the incorrect case
      */
-    public function testAfterFirstIncorrectCaseWithoutIgnore()
+    public function testAfterFirstCorrectCaseWithoutIgnore()
     {
         $str = new Str($this->testString);
         $str2 = $str->afterFirst('S');
         $this->assertInstanceOf(Str::class, $str2);
-        $this->assertTrue($str2->value !== ' over the lazy dog, Snowball');
+        $this->assertTrue($str2->value === 'nowball');
     }
 
     /**
      * Test whether it has the correct result after a string using the incorrect case with case-insensitive
      */
-    public function testAfterFirstIncorrectCaseWithIgnore()
+    public function testAfterFirstCorrectCaseWithIgnore()
     {
         $str = new Str($this->testString);
         $str2 = $str->afterFirst('S', true);
